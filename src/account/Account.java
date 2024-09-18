b3738d98f39654c1ab05a71f337df4f12b5a7d9c
Viggo2102 <146332112+Viggo2102@users.noreply.github.com>
2024-09-18 11:43:03 +0200

    Yesyes

--- src/account/Account.java
+++ src/account/Account.java
@@ -2,28 +2,41 @@ package account;
 
 import card.Card;
 import customer.Customer;
+import customer.ICustomer;
 
 public class Account implements IAccount {
-    private Customer customer;
-    private double balance;
-    private Card card;
+    ICustomer customer;
+    double balance;
 
-    public Account(Customer customer, double balance, Card card) {
+    public Account(ICustomer customer, double balance) {
         this.customer = customer;
         this.balance = balance;
-        this.card = card;
     }
 
-    public Customer getCustomer() {
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean withhdraw(double amount) {
+            if(amount>0 && amount<=getBalance()) {
+                balance -=amount;
+                System.out.println("Du har hævet: " + amount);
+                return true;
+
+            } else {
+                System.out.println("Beløbet du har valgt er ikke tilgængeligt");
+                return false;
+
+            }
+        }
+
+    public ICustomer getCustomer() {
         return customer;
     }
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
-    }
-
-    public double getBalance() {
-        return balance;
     }
 
     public void setBalance(double balance) {
